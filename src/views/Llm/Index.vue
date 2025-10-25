749da1853a851040261103e28dce5cc797cbbba4
six-key <2073958890@qq.com>
2025-10-25 14:34:48 +0800

    调整左侧历史会话样式

--- src/views/Llm/Index.vue
+++ src/views/Llm/Index.vue
@@ -1,23 +1,32 @@
 <template>
   <a-flex horizontal>
-    <div class="left">
-      <Left @activeChange="handleActiveChange" @createNewChat="handleCreateNewChat"
-        :newConversation="newConversation" />
+    <div :class="['left', { 'left-collapse': collapse }]">
+      <Left v-if="!collapse" @activeChange="handleActiveChange" @createNewChat="handleCreateNewChat"
+        @collapse="handleCollapse" :newConversation="newConversation" :collapse="collapse" />
+      <div v-else class="collapse-button">
+        <a-tooltip title="打开边栏" placement="bottom">
+          <a-button type="text" :icon="h(AlignLeftOutlined)" @click="() => { collapse = false }" />
+        </a-tooltip>
+        <a-tooltip title="开启新对话" placement="bottom">
+          <a-button type="text" :icon="h(PlusCircleOutlined)" @click="handleCreateNewChat" />
+        </a-tooltip>
+      </div>
     </div>
-    <div class="right">
+    <div :class="['right', { 'right-collapse': collapse }]">
       <Right :conversationId="conversationId" :createNewChat="createNewChat"
         @createNewChatSuccess="handleCreateNewChatSuccess" />
     </div>
   </a-flex>
 </template>
 <script setup>
-import { ref } from 'vue'
+import { ref, h } from 'vue'
+import { AlignLeftOutlined, PlusCircleOutlined } from '@ant-design/icons-vue'
 import Left from './components/Left.vue';
 import Right from './components/Right.vue';
 import { theme } from 'ant-design-vue';
 
 const { token } = theme.useToken();
-
+const collapse = ref(false)
 const conversationId = ref(null)
 const newConversation = ref({})
 const createNewChat = ref(false)
@@ -36,6 +45,9 @@ const handleCreateNewChat = () => {
 const handleActiveChange = (key) => {
   conversationId.value = key
 }
+const handleCollapse = (value) => {
+  collapse.value = value
+}
 </script>
 
 <style scoped lang="scss">
@@ -51,6 +63,29 @@ const handleActiveChange = (key) => {
   background: v-bind('token.colorFillTertiary');
   overflow-y: auto;
   border-right: 1px solid v-bind('token.colorBorder');
+  transition: all 0.2s ease-in-out;
+}
+
+.left-collapse {
+  width: 8%;
+  background: v-bind('token.colorBgContainer');
+  transition: all 0.2s ease-in-out;
+  border: none;
+
+  .collapse-button {
+    max-width: 70px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 10px;
+    padding: 2px;
+    border-radius: 50px;
+    border: 1px solid v-bind('token.colorBorder');
+
+    .ant-btn {
+      border-radius: 50px;
+    }
+  }
 }
 
 .right {
@@ -58,5 +93,9 @@ const handleActiveChange = (key) => {
   background: v-bind('token.colorBgContainer');
   min-height: 84vh;
   max-height: 88vh;
+}
+
+.right-collapse {
+  width: 92%;
 }
 </style>
--- src/views/Llm/components/Left.vue
+++ src/views/Llm/components/Left.vue
@@ -1,11 +1,26 @@
 <template>
   <div class="left-content">
     <div class="create-new-chat">
-      <a-button type="primary" size="large" block @click="handleCreateNewChat">新建对话</a-button>
+      <div class="pro_logo_option">
+        <a-flex justify="space-between">
+          <a-flex justify="horizontal" align="center">
+            <svg-icon name="vite" width="35" height="35" />
+            <span style="margin-left: 8px;font-size: 18px;font-weight: 500;">畅聊新天地</span>
+          </a-flex>
+          <a-flex justify="flex-end" align="center">
+            <a-button type="text" @click="handleCollapse">
+              <template #icon>
+                <AlignRightOutlined />
+              </template>
+            </a-button>
+          </a-flex>
+        </a-flex>
+      </div>
+      <a-button shape="round" size="large" :icon="h(PlusCircleOutlined)" block @click="handleCreateNewChat">开启新对话</a-button>
     </div>
 
     <div class="chat-history">
-      <AXConversations :activeKey="activeKey" :menu="menuConfig" :items="historyList" :style="style"
+      <AXConversations :activeKey="activeKey" :menu="menuConfig" :items="historyList"
         @activeChange="handleActiveChange" />
     </div>
   </div>
@@ -13,7 +28,7 @@
 
 <script setup>
 import { ref, onMounted, h, watch } from 'vue'
-import { EditOutlined, StopOutlined, DeleteOutlined } from '@ant-design/icons-vue'
+import { EditOutlined, PlusCircleOutlined, DeleteOutlined, PushpinOutlined, ShareAltOutlined } from '@ant-design/icons-vue'
 import { queryConversationHistory } from '@/api'
 import { message } from '@/utils'
 import { theme } from 'ant-design-vue';
@@ -23,33 +38,42 @@ const props = defineProps({
   newConversation: {
     type: Object,
     default: () => ({})
+  },
+  collapse: {
+    type: Boolean,
+    default: false
   }
 })
-const emit = defineEmits(['activeChange', 'createNewChat'])
+const emit = defineEmits(['activeChange', 'createNewChat', 'collapse'])
 
 const historyList = ref([])
 const { token } = theme.useToken();
 const appStore = useAppStore()
 const userStore = useUserStore()
-
 const activeKey = ref('')
 
 const menuConfig = (conversation) => ({
   items: [
     {
-      label: 'Operation 1',
+      label: '重命名',
       key: 'operation1',
       icon: h(EditOutlined),
     },
     {
-      label: 'Operation 2',
+      label: '置顶',
       key: 'operation2',
-      icon: h(StopOutlined),
+      icon: h(PushpinOutlined),
       disabled: false,
     },
     {
-      label: 'Operation 3',
+      label: '分享',
       key: 'operation3',
+      icon: h(ShareAltOutlined),
+      disabled: false,
+    },
+    {
+      label: '删除',
+      key: 'operation4',
       icon: h(DeleteOutlined),
       danger: true,
     },
@@ -58,13 +82,6 @@ const menuConfig = (conversation) => ({
     message.info(`Click ${conversation.key} - ${menuInfo.key}`);
   },
 });
-
-// const style = computed(() => ({
-//   width: '100%',
-//   height: '100%',
-//   background: token.value.colorFillTertiary,
-//   borderRadius: token.value.borderRadius,
-// }));
 
 const handleActiveChange = (key) => {
   emit('activeChange', key)
@@ -81,6 +98,11 @@ const handleQueryHistory = () => {
     historyList.value = res.data || []
   })
 }
+
+const handleCollapse = () => {
+  emit('collapse', true)
+}
+
 
 watch(() => props.newConversation, (newVal) => {
   if (newVal) {
@@ -108,14 +130,19 @@ onMounted(() => {
 
   .create-new-chat {
     padding: 0 6px;
-    height: 50px;
+    height: 106px;
     width: 100%;
     display: flex;
-    align-items: flex-end;
+    flex-direction: column;
+    justify-content: space-between;
+
+    .pro_logo_option {
+      line-height: 56px;
+    }
   }
 
   .chat-history {
-    height: calc(100% - 50px);
+    height: calc(100% - 106px);
     width: 100%;
     overflow-y: scroll;
   }
